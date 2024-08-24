920d4b1c6577c10d3d0e0310a054bb6009e1f645
dheeraj08mishra <dheeraj997mishra@gmail.com>
2024-08-24 14:18:28 +0530

    commit for code

--- Javascript/Basic Project/Counter/script.js
+++ Javascript/Basic Project/Counter/script.js
@@ -5,6 +5,9 @@ document.addEventListener("DOMContentLoaded",()=>{
     const changeByValue = document.querySelector("#changeBy");
     const counterValue = document.querySelector("#count");
     const btnReset = document.querySelector("#btnReset");
+    const lightTheme = document.querySelector("#lightTheme");
+    const darkTheme = document.querySelector('#darkTheme');
+    const nav = document.querySelector('.navBar');
     
     incrementBtn.addEventListener("click", () => {
         counterValue.innerText = parseInt(counterValue.innerText) + parseInt(changeByValue.value);
@@ -16,6 +19,18 @@ document.addEventListener("DOMContentLoaded",()=>{
     btnReset.addEventListener('click',()=>{
         counterValue.innerText = 0;
         changeByValue.value = 1;
+    })
+
+    lightTheme.addEventListener('click',()=>{
+        document.body.style.backgroundColor = 'white';
+        document.body.style.color = 'black';
+        document.querySelector('h1').style.color = 'black';
+        
+    });
+    darkTheme.addEventListener('click',()=>{
+        document.body.style.backgroundColor = 'black';
+        document.body.style.color = 'white';
+        document.querySelector('h1').style.color = 'black';
     })
 })
 
